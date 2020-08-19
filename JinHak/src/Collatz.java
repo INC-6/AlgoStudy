bdb801a0df0794daec9858aa7e571b3dc579cf87
weejinhak <wlsgkr91@gmail.com>
2020-08-19 17:34:45 +0900

    Apply .gitignore

---
+++ JinHak/src/Collatz.java
@@ -0,0 +1,28 @@
+public class Collatz {
+    public int solution(long num) {
+        int answer = 0;
+
+        int count=0;
+        while(true){
+            if(count > 500){
+                answer = -1;
+                break;
+            }
+            if(num==1){
+                answer = count;
+                break;
+            }
+            if(num%2==0){
+                count++;
+                num = num/2;
+                continue;
+            }
+            if(num%2==1){
+                count++;
+                num = (num*3)+1;
+                continue;
+            }
+        }
+        return answer;
+    }
+}
