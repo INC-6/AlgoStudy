bdff8e15e4efc614be00a10e6bcac8b521437db4
weejinhak <wlsgkr91@gmail.com>
2020-08-19 17:56:08 +0900

    1주차 추가 문제 업로드

---
+++ JinHak/src/week1_problem.java
@@ -0,0 +1,28 @@
+public class week1_problem {
+
+    static int num = 0;
+    static int[] intArr = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            cut(i);
+        }
+        for (int i = 0 ; i < intArr.length; i++){
+            System.out.println(i+"의 개수" + intArr[i]);
+        }
+    }
+
+    private static void cut(int i) {
+        num = i;
+        System.out.println("잘라야할 숫자 " + i);
+        while (num > 0) {
+            System.out.println("자른 숫자 " + num % 10);
+            CntCalculation(num % 10);
+            num /= 10;
+        }
+    }
+
+    private static void CntCalculation(int i) {
+        intArr[i] = intArr[i] + 1;
+    }
+}
