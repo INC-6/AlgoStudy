3dac0d68e7281ae7563675f681976aa25d3077d8
SYL <alphasuno214@gmail.com>
2020-08-30 16:28:49 +0900

    1번 문제 코드 정리

--- SoYeon/week2/week2_problem_1.java
+++ SoYeon/week2/week2_problem_1.java
@@ -9,19 +9,19 @@ class Solution {
         for(int[] arr : commands) {
             //자르기 & 새로운 배열에 담기
             System.out.println("자르기 & 새로운 배열에 담기");
-            int[] sortedArray = new int[arr[1]-arr[0]+1];
+            int[] newArray = new int[arr[1]-arr[0]+1];
             for(int i = arr[0]-1, j = 0; i < arr[1]; i++, j++) {
 //                System.out.println(array[i]);
-                sortedArray[j] = array[i];
+                newArray[j] = array[i];
             }
 
             //정렬
             System.out.println("정렬");
-            Arrays.sort(sortedArray);
+            Arrays.sort(newArray);
 
             //정답 배열에 담기
             System.out.println("정답배열에 담기");
-            answer[k] = sortedArray[arr[2]-1];
+            answer[k] = newArray[arr[2]-1];
 //            System.out.println("자릿수 : "+arr[2]);
 //            System.out.println("해당 숫자"+answer[k]);
             k++;
