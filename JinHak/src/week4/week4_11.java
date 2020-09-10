142e62e0b8748331249d31cbcb660a8ec990d484
weejinhak <wlsgkr91@gmail.com>
2020-09-10 22:56:06 +0900

    Commit week_11

--- JinHak/src/week4/week4_11.java
+++ JinHak/src/week4/week4_11.java
@@ -1,45 +1,89 @@
 package week4;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class week4_11 {
 
-    static int[] A = {3, 2, -2, 5, -3};
-    static int[] B = {1, 1, 2, -1, 2, -1};
-    static int[] C = {1, 2, 3, -4};
+    private static String[] chkPoint = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_"};
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        solution(A);
-        solution(B);
-        solution(C);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        System.out.println(solution(input));
 
     }
 
-    //출력함수
-    private static void solution(int[] arr) {
-        //배열 없거나 1개면 그대로 0
-        if (arr.length == 0 || arr.length == 1) System.out.println(0);
+    //validationCheck
+    private static boolean solution(String input) {
 
-        int result = 0;
+        String[] check01 = input.split("");
 
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] > 0) { //배열의 값이 양수면,
-
-                for (int j = 0; j < arr.length; j++) {
-
-                    if (arr[i] + arr[j] == 0) {
-                        //A배열 두개 나옴 두개 중 큰거
-
-                        if (arr[i] > result) result = arr[i];
-
-                    }
-
-                }
-
-            }
+        //1. 길이체크 6보다 커야됨
+        if (check01.length < 6) {
+            System.out.println("길이가 6보다 작음");
+            return false;
         }
 
-        System.out.println(result);
+        //2. 공백체크
+        String[] check02 = input.split(" ");
+        if (check02.length >= 2) {
+            System.out.println("공백이 포함되어 있음");
+            return false;
+        }
+
+
+        //3. 특수문자 개수 체크 특수문자 한개도 없으면 return false;
+        String[] check03 = input.split("");
+        ArrayList<String> strArrayList = new ArrayList<>();
+        int chkPointCnt = 0;
+        for (String chkStr : check03) {
+
+            //숫자나 특수문자가 아닌 애들 담기 아래서 대문자 소문자 갯수판별예정
+            if (!checkNumber(chkStr))
+                strArrayList.add(chkStr);
+
+            for (String aChkPoint : chkPoint) {
+                if (chkStr.equals(aChkPoint)) chkPointCnt++;
+            }
+
+        }
+        if(chkPointCnt < 1) {
+            System.out.println("특수문자가 없음");
+            return false;
+        }
+
+
+        //대소문자 각각 1개씩 있는지 체크
+        int upperCnt = 0;
+        int lowerCnt = 0;
+        for(String chkStr : strArrayList){
+            if(Character.isUpperCase(chkStr.charAt(0))) upperCnt++; //대문자이면 대문자 개수 증가
+            if(Character.isLowerCase(chkStr.charAt(0))) lowerCnt++; //소문자이면 소문자 개수 증가
+        }
+
+        if(upperCnt < 1 || lowerCnt < 1) {
+            System.out.println("대소문자 1개이상씩 들어가야됨");
+            return false;
+        }
+
+        //위의 모든 과정에서 false로 빠지지 않고 통과하면 return true;
+        return true;
+    }
+
+
+    //numberCheck
+    private static boolean checkNumber(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
 
     }
 
